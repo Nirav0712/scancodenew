1d52956b4f1894d67656ed1d1675c871be61848f
mrmajithiya <oom863572@gmail.com>
2026-07-04 02:22:26 +0530

    changees

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,10 @@
-import HeroSection from "./components/Herosection";
-import CategoriesSection from "./components/CategoriesSection";
-import AboutSection from "./components/AboutSection";
-import ServicesSection from "./components/ServicesSection";
-import MarqueeBanner from "./components/MarqueeBanner";
-import WhyChooseUs from "./components/WhyChooseUs";
-import FeaturesFan from "./components/FeaturesFan";
-import CustomPrintServices from "./components/CustomPrintServices";
-import PartnersSection from "./components/PartnersSection";
-// import TeamSection from "./components/TeamSection";
-import TestimonialsSection from "./components/TestimonialsSection";
+
 // import Cardsscroll from "./components/Cardsscroll";
-import FancySlider from "./components/FancySlider";
 
 export default function Home() {
   return (
     <>
-      <FancySlider />
-      {/* <HeroSection /> */}
-      <CategoriesSection />
-      <AboutSection />
-      <ServicesSection />
-      <MarqueeBanner />
-      <WhyChooseUs />
-      <FeaturesFan />
-      <CustomPrintServices />
-      {/* <PartnersSection /> */}
-      {/* <TeamSection /> */}
-      <TestimonialsSection />
-      {/* <Cardsscroll /> */}
+
     </>
   );
 }
